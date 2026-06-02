43ef09e2bd69bae79957cf57f2123c0b45a019a1
rahultechles007 <binodkumarpradhan145@gmail.com>
2026-06-02 22:57:15 +0530

    last commit

---
+++ Day_1/script.js
@@ -0,0 +1,7 @@
+
+
+function showMessage() {
+            document.getElementById("message").innerHTML =
+                "Congratulations! Your first web page is working successfully.";
+        }
+
