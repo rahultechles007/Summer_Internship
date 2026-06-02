1ede5e620f5089a5e9c185b76491b231f3633316
rahultechles007 <binodkumarpradhan145@gmail.com>
2026-06-02 22:26:51 +0530

    Third commit

--- Day_5/script.js
+++ Day_5/script.js
@@ -1,3 +1,59 @@
-function toggleMenu() {
-    document.getElementById("navMenu").classList.toggle("show");
+const menuBtn = document.getElementById("menuBtn");
+const navMenu = document.getElementById("navMenu");
+menuBtn.addEventListener("click", () => {
+
+    navMenu.classList.toggle("active");
+
+    menuBtn.textContent =
+    navMenu.classList.contains("active")
+    ? "✖"
+    : "☰";
+});
+document
+.querySelectorAll(".nav-links a")
+.forEach(link => {
+    link.addEventListener("click", () => {
+        if(window.innerWidth < 1024){
+            navMenu.classList.remove("active");
+            menuBtn.textContent = "☰";
+        }
+    });
+});
+function revealSections(){
+    const reveals =
+    document.querySelectorAll(".reveal");
+    reveals.forEach(section => {
+        const top =
+        section.getBoundingClientRect().top;
+
+        const visible =
+        window.innerHeight - 100;
+
+        if(top < visible){
+
+            section.classList.add("active");
+        }
+
+    });
 }
+window.addEventListener(
+"scroll",
+revealSections
+);
+revealSections();
+window.addEventListener("scroll", () => {
+
+    const header =
+    document.querySelector("header");
+
+    if(window.scrollY > 50){
+
+        header.style.boxShadow =
+        "0 4px 15px rgba(0,0,0,.2)";
+    }else{
+        header.style.boxShadow =
+        "none";
+
+    }
+
+});
